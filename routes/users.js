b3364bc9005d1426bc666ab5bd1385dcf03fcd66
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-11-04 22:31:35 +0530

    Initial Commit

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,9 @@
-const mongoose = require("mongoose")
+var express = require('express');
+var router = express.Router();
 
-// Connect to local host mongodb server 
-mongoose.connect("mongodb://127.0.0.1:27017/amazonDB")
-// The above line create a database in mongodb named "amazonDB"
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-const userSchema = mongoose.Schema({
-  username: String,
-  name: String,
-  age: Number
-})
-
-module.exports = mongoose.model("userDB", userSchema)
+module.exports = router;
